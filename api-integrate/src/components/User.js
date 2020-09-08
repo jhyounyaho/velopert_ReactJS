da4d9a5f58d9b3f187908ce1c6cb578d5eeda89f
jhyounyaho <jhyoun-yaho@hanmail.net>
2020-09-08 14:46:42 +0900

    react-async로 요청 상태 관리하기

--- api-integrate/src/components/User.js
+++ api-integrate/src/components/User.js
@@ -1,8 +1,21 @@
+/*
+  react-async로 요청 상태 관리하기 
+  yarn add react-async 
+
+  장점
+  - 필요할때 설치하여 사용가능 
+  - 비동기 기능 탑재
+  - hook, component 형태 
+  - 특정 promise 도중 취소 가능 
+
+  단점 
+  - option 조금 복잡 
+*/
 import React from "react";
 import axios from "axios";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 
-async function getUser(id) {
+async function getUser({ id }) {
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/users/${id}`
   );
@@ -11,10 +24,13 @@ async function getUser(id) {
 
 function User({ id }) {
   // id값 바뀔때마다 useAsync 호출
-  const [state] = useAsync(() => getUser(id), [id]);
+  const { data: user, error, isLoading } = useAsync({
+    promiseFn: getUser,
+    id,
+    watch: id,
+  });
 
-  const { loading, error, data: user } = state;
-  if (loading) return <div>로딩중 ...</div>;
+  if (isLoading) return <div>로딩중 ...</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!user) return null;
 
--- api-integrate/src/components/Users.js
+++ api-integrate/src/components/Users.js
@@ -1,12 +1,9 @@
 /*
   useAsync 커스텀 Hook 만들어서 사용하기 
-
-  react-async로 요청 상태 관리하기 
-  yarn add react-async 
 */
 import React, { useState } from "react";
 import axios from "axios";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 import User from "./User";
 
 // useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
@@ -19,13 +16,15 @@ async function getUsers() {
 }
 
 function Users() {
-  const [state, refetch] = useAsync(getUsers, [], true);
   const [userId, setUserId] = useState(null);
+  const { data: users, error, isLoading, reload, run } = useAsync({
+    deferFn: getUsers,
+    //promiseFn: getUsers,
+  });
 
-  const { loading, error, data: users } = state;
-  if (loading) return <div>로딩중 ...</div>;
+  if (isLoading) return <div>로딩중 ...</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
+  if (!users) return <button onClick={run}>불러오기</button>;
 
   return (
     <>
@@ -36,7 +35,7 @@ function Users() {
           </li>
         ))}
       </ul>
-      <button onClick={refetch}>다시 불러오기</button>
+      <button onClick={reload}>다시 불러오기</button>
       {userId && <User id={userId} />}
     </>
   );
