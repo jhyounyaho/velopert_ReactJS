c57f45c9ddcc4be4ebb828635aa63bdffe0bae0a
jhyounyaho <jhyoun-yaho@hanmail.net>
2020-09-10 21:30:59 +0900

    shallowEqual 로 useSelector 최적화하기

--- learn-redux/src/containers/CounterContainer.js
+++ learn-redux/src/containers/CounterContainer.js
@@ -8,14 +8,28 @@
 */
 import React from "react";
 import Counter from "../components/Counter";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { increase, decrease, setDiff } from "../modules/counter";
 
 function CounterContainer() {
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual
+    /*
+    (left, right) => {
+      return left.diff === right.diff && left.number === right.number;
+    }
+    */
+  );
+  /*
+  // useSelector 최적화를 위해 분리함.
+  // TODO 체크방법 작업자 도구 > Profiler 확인
+  const number = useSelector((state) => state.counter.number);
+  const diff = useSelector((state) => state.counter.diff);
+  */
   const dispatch = useDispatch();
 
   const onIncrease = () => dispatch(increase());
